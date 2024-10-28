693874f8573e7bd093a33cb804a8d2f57d0ec420
William Kermer <William.Kermer21@gmail.com>
2024-10-28 01:24:57 -0300

    Create PedraAr.java

---
+++ Exercises/ex007/PedraAr.java
@@ -0,0 +1,10 @@
+public class PedraAr extends PedraElemento {
+    
+    public PedraAr() {
+        // Usando os métodos set para atribuir os valores
+        setNome("Pedra de Ar");
+        setElemento("Ar");
+        setNivelDeForca(300);
+        setCor("Branca");
+    }
+}
