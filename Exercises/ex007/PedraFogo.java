5fecb4536580bafcb8c953a77beb834a2cf3c283
William Kermer <William.Kermer21@gmail.com>
2024-10-28 01:24:41 -0300

    Create PedraFogo.java

---
+++ Exercises/ex007/PedraFogo.java
@@ -0,0 +1,10 @@
+public class PedraFogo extends PedraElemento {
+    
+    public PedraFogo() {
+        // Usando os métodos set para atribuir os valores
+        setNome("Pedra de Fogo");
+        setElemento("Fogo");
+        setNivelDeForca(100);
+        setCor("Vermelha");
+    }
+}
