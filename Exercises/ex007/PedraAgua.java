d48500893988d05891cce63e6a1bbd2643d67224
William Kermer <William.Kermer21@gmail.com>
2024-10-28 01:24:49 -0300

    Create PedraAgua.java

---
+++ Exercises/ex007/PedraAgua.java
@@ -0,0 +1,10 @@
+public class PedraAgua extends PedraElemento {
+    
+    public PedraAgua() {
+        // Usando os métodos set para atribuir os valores
+        setNome("Pedra de Agua");
+        setElemento("Agua");
+        setNivelDeForca(200);
+        setCor("Azul");
+    }
+}
