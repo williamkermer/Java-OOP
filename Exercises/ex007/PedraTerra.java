d0418ee8b12069b6c1f7b4c52d03bfa5e9a69233
William Kermer <William.Kermer21@gmail.com>
2024-10-28 01:25:08 -0300

    Create PedraTerra.java

---
+++ Exercises/ex007/PedraTerra.java
@@ -0,0 +1,10 @@
+public class PedraTerra extends PedraElemento {
+    
+    public PedraTerra() {
+        // Usando os métodos set para atribuir os valores
+        setNome("Pedra de Terra");
+        setElemento("Terra");
+        setNivelDeForca(250);
+        setCor("Marrom");
+    }
+}
