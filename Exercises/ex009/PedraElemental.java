236e1d1da9cd520764a08fb8badd24ee9d35c406
William Kermer <William.Kermer21@gmail.com>
2024-11-18 15:00:28 -0300

    Create PedraElemental.java

---
+++ Exercises/ex009/PedraElemental.java
@@ -0,0 +1,21 @@
+public class PedraElemental{
+    
+private int poderNucleo;
+private int poderTotal;
+    
+    public int getPoderNucleo() {
+    return poderNucleo;
+    }
+
+    public void setPoderNucleo(int poderNucleo) {
+        this.poderNucleo = poderNucleo;
+    }
+    
+    public int getPoderTotal() {
+    return poderTotal;
+    }
+    
+    public void setPoderTotal(int poderTotal) {
+        this.poderTotal = poderTotal;
+    }
+}
