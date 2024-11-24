c1332622fdabd72e20162bc3ba20fce79b1126c9
William Kermer <William.Kermer21@gmail.com>
2024-11-24 02:14:12 -0300

    Create Espada.java

---
+++ Exercises/ex010/Espada.java
@@ -0,0 +1,23 @@
+public class Espada {
+
+    private int forcaFisica;
+    private String nome;
+
+    // Getter e Setter para ForcaFisica
+    public int getForcaEspada() {
+        return forcaFisica;
+    }
+
+    public void setForcaEspada(int forcaFisica) {
+        this.forcaFisica = forcaFisica;
+    }
+
+    // Getter e Setter para Nome
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
